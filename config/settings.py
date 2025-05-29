db230eafbd938f08da9f3792e307d9c4b85beea8
MateuszSokolowski03 <eusmateusz@gmail.com>
2025-05-29 19:29:07 +0200

    Poprawka zwiazana z linkiem do strony

--- config/settings.py
+++ config/settings.py
@@ -139,3 +139,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://planer-zespolow.up.railway.app'
+]
