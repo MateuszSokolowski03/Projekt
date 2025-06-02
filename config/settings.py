7d4ef177cebfc4b2dfd8124da67435b5249e027f
kacperDebowski <167117372+kacperDebowski@users.noreply.github.com>
2025-06-02 22:44:58 +0200

    doddanie template do panelu admina

--- config/settings.py
+++ config/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = ['.railway.app', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -67,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request',
             ],
         },
     },
--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,7 @@ from apps.planner.views import register_view, login_view
 from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
