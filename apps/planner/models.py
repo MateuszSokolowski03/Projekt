7cdbb2aec32a0d60fc3c29799e396497c723d571
MateuszSokolowski03 <eusmateusz@gmail.com>
2025-06-08 12:55:03 +0200

    Match_event musi mieć wybranego piłkarza

--- apps/planner/models.py
+++ apps/planner/models.py
@@ -118,7 +118,7 @@ class MatchEvent(models.Model):
         ('red_card', 'Red Card'),
     ]
     event_type = models.CharField(max_length=20, choices=EVENT_TYPES)
-    player = models.ForeignKey('Player', on_delete=models.CASCADE, null=True, blank=True)
+    player = models.ForeignKey('Player', on_delete=models.CASCADE)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='match_events') 
 
     def __str__(self):
